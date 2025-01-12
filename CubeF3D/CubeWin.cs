06700fae4808e878fd5d96252980adaf776c42b8
Mădean Denis <123639454+madeandenis@users.noreply.github.com>
2025-01-13 01:31:32 +0200

    Add project files.

---
+++ CubeF3D/CubeWin.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CubeF3D
+{
+    public enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public class Angle
+    {
+        private float _x;
+        private float _y;
+        private float _z;
+
+        public float X
+        {
+            get => _x;
+            set => _x = Normalize(value);
+        }
+        public float Y
+        {
+            get => _y;
+            set => _y = Normalize(value);
+        }
+
+        public float Z
+        {
+            get => _z;
+            set => _z = Normalize(value);
+        }
+
+        public Angle(float x = 0, float y = 0, float z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        private float Normalize(float angle)
+        {
+            angle = angle % 360; 
+            if (angle < 0)
+            {
+                angle += 360;
+            }
+            return angle;
+        }
+    }
+
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public Point(float x, float y, float z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static Point TransformTo2D(Point point, Size size)
+        {
+            // Perspective Projection
+            float d = 5.0f; // distance from the camera to the scene (viewer's distance)
+
+            // Perspective transformation (with a simple scaling based on z)
+            float x2D = point.X / (1 + point.Z / d);
+            float y2D = point.Y / (1 + point.Z / d);
+
+            // Scale and translate to fit in the screen
+            x2D = x2D * size.Width / 2 + size.Width / 2;
+            y2D = -y2D * size.Height / 2 + size.Height / 2;
+
+            return new Point(x2D, y2D);
+        }
+
+    }
+
+    public class Cube
+    {
+        private Form form;
+        
+        public Angle rotationAngle;
+
+        public float RotationSpeed;
+
+        public float ScaleFactor;
+        private const float MaxScaleFactor = 1.5f;  
+        private const float MinScaleFactor = 0.2f;  
+
+        private Point[] vertices3D;
+        private Point[] vertices2D;
+
+        public Boolean highlightedVertices = false;
+        public float vEllipseSize = 20;
+
+        public Color edgesColor = Color.ForestGreen;
+
+        public Axis currentAxis = Axis.Y;
+
+
+        public Cube(Form form, float scale = 1f, float rotationSpeed = 0.1f)
+        {
+            this.form = form;
+
+            vertices3D = new Point[]
+            {
+                new Point(-1, -1, -1), new Point( 1, -1, -1), new Point( 1,  1, -1), new Point(-1,  1, -1),
+                new Point(-1, -1,  1), new Point( 1, -1,  1), new Point( 1,  1,  1), new Point(-1,  1,  1)
+            };
+            rotationAngle = new Angle();
+            ScaleFactor = scale;
+            RotationSpeed = rotationSpeed;
+        }
+
+        private Point[] ApplyTransformations(Point[] vertices)
+        {
+            Matrix4 scalingMatrix = Matrix4.ScalingMatrix(ScaleFactor, ScaleFactor, ScaleFactor);
+            Matrix4 rotationMatrix;
+            switch (currentAxis)
+            {
+                case Axis.X:
+                    rotationMatrix = Matrix4.RotationMatrix_X(rotationAngle.X);
+                    break;
+                case Axis.Y:
+                    rotationMatrix = Matrix4.RotationMatrix_Y(rotationAngle.Y);
+                    break;
+                case Axis.Z:
+                    rotationMatrix = Matrix4.RotationMatrix_Z(rotationAngle.Z);
+                    break;
+                default:
+                    rotationMatrix = Matrix4.IdentityMatrix;
+                    break;
+            }
+
+            Point[] transformedVertices = new Point[vertices.Length];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                transformedVertices[i] = Matrix4.ApplyToPoint(rotationMatrix, vertices[i]);
+                transformedVertices[i] = Matrix4.ApplyToPoint(scalingMatrix, transformedVertices[i]);
+            }
+
+            return transformedVertices;
+        }
+
+        public void Render(PaintEventArgs e)
+        {
+            Point[] transformedVertices = ApplyTransformations(vertices3D);
+
+            vertices2D = new Point[transformedVertices.Length];
+            for (int i = 0; i < transformedVertices.Length; i++)
+            {
+                vertices2D[i] = Point.TransformTo2D(transformedVertices[i], form.ClientSize);
+            }
+
+            DrawEdges(e.Graphics);
+            DrawHighlightedVertices(e.Graphics);
+        }
+
+        private void DrawHighlightedVertices(Graphics g)
+        {
+            if (highlightedVertices)
+            {
+                Font font = new Font("Arial", 12, FontStyle.Bold); 
+                Brush textBrush = Brushes.White;
+
+                Color startColor = Color.Blue;
+                Color endColor = Color.Red;
+
+
+                for (int i = 0; i < vertices2D.Length; i++)
+                {
+                    var vertex = vertices2D[i];
+
+                    float colorRatio = (float)i / (vertices2D.Length - 1); 
+                    Color vertexColor = InterpolateColor(startColor, endColor, colorRatio); 
+
+                    Brush brush = new SolidBrush(vertexColor);
+
+                    g.FillEllipse(brush, vertex.X - vEllipseSize / 2, vertex.Y - vEllipseSize / 2, vEllipseSize, vEllipseSize);
+
+                    string indexText = i.ToString(); 
+                    SizeF textSize = g.MeasureString(indexText, font); 
+                    float textX = vertex.X - textSize.Width / 2; 
+                    float textY = vertex.Y - textSize.Height / 2; 
+
+                    g.DrawString(indexText, font, textBrush, textX, textY);
+                }
+            }
+        }
+
+        private void DrawEdges(Graphics g)
+        {
+            Pen pen = new Pen(edgesColor);
+            DrawEdge(g, pen, 0, 1); DrawEdge(g, pen, 1, 2); DrawEdge(g, pen, 2, 3); DrawEdge(g, pen, 3, 0); // Back 
+            DrawEdge(g, pen, 4, 5); DrawEdge(g, pen, 5, 6); DrawEdge(g, pen, 6, 7); DrawEdge(g, pen, 7, 4); // Front
+            DrawEdge(g, pen, 0, 4); DrawEdge(g, pen, 1, 5); DrawEdge(g, pen, 2, 6); DrawEdge(g, pen, 3, 7); // Front & Back
+        }
+
+        private void DrawEdge(Graphics g, Pen pen, int startIdx, int endIdx)
+        {
+            g.DrawLine(
+                pen, 
+                vertices2D[startIdx].X, vertices2D[startIdx].Y,
+                vertices2D[endIdx].X, vertices2D[endIdx].Y
+            );
+        }
+
+        private Color InterpolateColor(Color startColor, Color endColor, float colorRatio)
+        {
+            int r = (int)(startColor.R + colorRatio * (endColor.R - startColor.R));
+            int g = (int)(startColor.G + colorRatio * (endColor.G - startColor.G));
+            int b = (int)(startColor.B + colorRatio * (endColor.B - startColor.B));
+
+            return Color.FromArgb(r, g, b); 
+        }
+
+    }
+
+    public partial class CubeWin : Form
+    {
+        Cube Cube { get; set; }
+
+        private Timer timer;
+        private int FPS = 60;
+        private bool paused = false;
+
+        public CubeWin()
+        {
+            InitializeComponent();
+
+            Width = 600;
+            Height = 600;
+            BackColor = Color.Black;
+            
+            this.DoubleBuffered = true; 
+            this.MouseWheel += CubeWin_MouseWheel;
+
+            Cube = new Cube(this, scale: 1f);
+            Cube.highlightedVertices = true;
+            Cube.vEllipseSize = 40;
+
+            InitializeTimer();
+
+        }
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000 / FPS;
+            timer.Tick += (sender, e) =>
+            {
+                if (!paused) 
+                {
+                    Cube.rotationAngle.X += Cube.RotationSpeed;
+                    Cube.rotationAngle.Y += Cube.RotationSpeed;
+                    Cube.rotationAngle.Z += Cube.RotationSpeed;
+                    Invalidate(); 
+                }
+            };
+            timer.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Cube.Render(e);
+        }
+
+        private void CubeWin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.X)
+            {
+                Cube.currentAxis = Axis.X; 
+            }
+            else if (e.KeyCode == Keys.Y)
+            {
+                Cube.currentAxis = Axis.Y; 
+            }
+            else if (e.KeyCode == Keys.Z)
+            {
+                Cube.currentAxis = Axis.Z; 
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                paused = !paused; 
+            }
+        }
+
+        private void CubeWin_MouseWheel(object sender, MouseEventArgs e)
+        {
+            Cube.ScaleFactor += e.Delta > 0 ? 0.1f : -0.1f;
+        }
+    }
+}
---
+++ CubeF3D/Matrix4.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace CubeF3D
+{
+    public class Matrix4
+    {
+        public float M11, M12, M13, M14;
+        public float M21, M22, M23, M24;
+        public float M31, M32, M33, M34;
+        public float M41, M42, M43, M44;
+
+        public Matrix4(
+            float m11, float m12, float m13, float m14,
+            float m21, float m22, float m23, float m24,
+            float m31, float m32, float m33, float m34,
+            float m41, float m42, float m43, float m44)
+        {
+            M11 = m11; M12 = m12; M13 = m13; M14 = m14;
+            M21 = m21; M22 = m22; M23 = m23; M24 = m24;
+            M31 = m31; M32 = m32; M33 = m33; M34 = m34;
+            M41 = m41; M42 = m42; M43 = m43; M44 = m44;
+        }
+
+        public static Matrix4 Multiply(Matrix4 a, Matrix4 b)
+        {
+            return new Matrix4(
+                a.M11 * b.M11 + a.M12 * b.M21 + a.M13 * b.M31 + a.M14 * b.M41,
+                a.M11 * b.M12 + a.M12 * b.M22 + a.M13 * b.M32 + a.M14 * b.M42,
+                a.M11 * b.M13 + a.M12 * b.M23 + a.M13 * b.M33 + a.M14 * b.M43,
+                a.M11 * b.M14 + a.M12 * b.M24 + a.M13 * b.M34 + a.M14 * b.M44,
+
+                a.M21 * b.M11 + a.M22 * b.M21 + a.M23 * b.M31 + a.M24 * b.M41,
+                a.M21 * b.M12 + a.M22 * b.M22 + a.M23 * b.M32 + a.M24 * b.M42,
+                a.M21 * b.M13 + a.M22 * b.M23 + a.M23 * b.M33 + a.M24 * b.M43,
+                a.M21 * b.M14 + a.M22 * b.M24 + a.M23 * b.M34 + a.M24 * b.M44,
+
+                a.M31 * b.M11 + a.M32 * b.M21 + a.M33 * b.M31 + a.M34 * b.M41,
+                a.M31 * b.M12 + a.M32 * b.M22 + a.M33 * b.M32 + a.M34 * b.M42,
+                a.M31 * b.M13 + a.M32 * b.M23 + a.M33 * b.M33 + a.M34 * b.M43,
+                a.M31 * b.M14 + a.M32 * b.M24 + a.M33 * b.M34 + a.M34 * b.M44,
+
+                a.M41 * b.M11 + a.M42 * b.M21 + a.M43 * b.M31 + a.M44 * b.M41,
+                a.M41 * b.M12 + a.M42 * b.M22 + a.M43 * b.M32 + a.M44 * b.M42,
+                a.M41 * b.M13 + a.M42 * b.M23 + a.M43 * b.M33 + a.M44 * b.M43,
+                a.M41 * b.M14 + a.M42 * b.M24 + a.M43 * b.M34 + a.M44 * b.M44
+            );
+        }
+
+        public static Point ApplyToPoint(Matrix4 matrix, Point point)
+        {
+            float x = matrix.M11 * point.X + matrix.M12 * point.Y + matrix.M13 * point.Z + matrix.M14;
+            float y = matrix.M21 * point.X + matrix.M22 * point.Y + matrix.M23 * point.Z + matrix.M24;
+            float z = matrix.M31 * point.X + matrix.M32 * point.Y + matrix.M33 * point.Z + matrix.M34;
+
+            return new Point(x, y, z);
+        }
+
+        public static Matrix4 IdentityMatrix => new Matrix4(
+              1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1, 0,
+              0, 0, 0, 1
+        );
+
+
+        public static Matrix4 ScalingMatrix(float sx, float sy, float sz)
+        {
+            return new Matrix4(
+                sx, 0, 0, 0,
+                0, sy, 0, 0,
+                0, 0, sz, 0,
+                0, 0, 0, 1
+            );
+        }
+
+        public static Matrix4 RotationMatrix_X(float angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new Matrix4(
+                1, 0, 0, 0,
+                0, cos, -sin, 0,
+                0, sin, cos, 0,
+                0, 0, 0, 1
+            );
+        }
+        public static Matrix4 RotationMatrix_Y(float angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new Matrix4(
+                cos, 0, sin, 0,
+                0, 1, 0, 0,
+                -sin, 0, cos, 0,
+                0, 0, 0, 1
+            );
+        }
+        public static Matrix4 RotationMatrix_Z(float angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new Matrix4(
+                cos, -sin, 0, 0,
+                sin, cos, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1
+            );
+        }
+
+    }
+}
